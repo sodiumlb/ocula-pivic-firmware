1f76ec8b25e7b88fd4c8af4ba4c36a837443a94a
Sodiumlightbaby <edvard@xox.no>
2025-03-24 08:27:31 +0100

    Setup Move str and mem modules

--- src/ocula/sys/mem.h
+++ src/ocula/sys/mem.h
@@ -7,18 +7,37 @@
 #ifndef _MEM_H_
 #define _MEM_H_
 
+#include <stddef.h>
 #include <stdint.h>
-
-#define XRAM_RW0 REGS(0xFFE4)
-#define XRAM_STEP0 *(int8_t *)&REGS(0xFFE5)
-#define XRAM_ADDR0 REGSW(0xFFE6)
-
-#define XRAM_RW1 REGS(0xFFE8)
-#define XRAM_STEP1 *(int8_t *)&REGS(0xFFE9)
-#define XRAM_ADDR1 REGSW(0xFFEA)
+#include <stdbool.h>
 
 // 64KB Extended RAM
-extern volatile const uint8_t xram[0x10000];
-asm(".equ xram, 0x20030000");
+#ifdef NDEBUG
+extern uint8_t xram[0x10000];
+#else
+extern uint8_t *const xram;
+#endif
+
+// The xstack is:
+// 512 bytes, enough to hold a CC65 stack frame, two strings for a
+// file rename, or a disk sector
+// 1 byte at end+1 always zero for cstring and safety.
+// Using xstack for cstrings doesn't require sending the zero termination.
+#define XSTACK_SIZE 0x200
+extern uint8_t xstack[];
+extern volatile size_t xstack_ptr;
+
+// RIA registers are located at the bottom of cpu1 stack.
+// cpu1 runs the action loop and uses very little stack.
+extern uint8_t regs[0x20];
+#define REGS(addr) regs[(addr) & 0x1F]
+#define REGSW(addr) ((uint16_t *)&REGS(addr))[0]
+asm(".equ regs, 0x20040000");
+
+// Misc memory buffer for moving things around.
+// 6502 <-> RAM, USB <-> RAM, UART <-> RAM, etc.
+#define MBUF_SIZE 1024
+extern uint8_t mbuf[];
+extern size_t mbuf_len;
 
 #endif /* _MEM_H_ */
