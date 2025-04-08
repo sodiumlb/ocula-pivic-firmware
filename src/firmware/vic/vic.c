ab1f5174722ef915926e8a8e4bc1dc7494f1bcc9
Lance Ewing <lance.ewing@gmail.com>
2025-04-08 20:27:04 +0100

    Started laying out some logic to support pixel shift register.

--- src/firmware/vic/vic.c
+++ src/firmware/vic/vic.c
@@ -231,6 +231,21 @@ void core1_entry(void) {
             videoMatrixCounter++;
         }
 
+        // At the end of F1, if 'In Matrix', then the data read from memory arrives.
+        if (inMatrix) {
+            if (horizontalCellCounter & 1) {
+                // TODO: This is where we read the cell index from VIC PIO.
+                cellIndex = 0;
+            }
+            else {
+                // TODO: This is where we read the char data from VIC PIO.
+                charData = 0b10101011;
+            }
+        }
+
+        // TODO: Top bit of pixel shift register comes out immediately.
+
+
 
         // TODO: Should we poll here for phase 2 via another interrupt? e.g. irq 1
 
@@ -330,7 +345,7 @@ void core1_entry(void) {
             matrixLine = true;
         }
 
-        // TODO: Calculate address to fetch in next F1.
+        // TODO: If 'In Matrix', calculate address to fetch in next F1.
 
 
         // TODO: Should this be cleared at end of loop? Or immediately after polling?
