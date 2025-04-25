60042c2530b7e7f9862bb3e3f94754208f246b00
Sodiumlightbaby <edvard@xox.no>
2025-04-25 18:52:01 +0200

    Fix Make monitor RAM operations function with extended xram

--- src/firmware/mon/ram.c
+++ src/firmware/mon/ram.c
@@ -16,42 +16,12 @@
 
 static enum {
     SYS_IDLE,
-    SYS_READ,
-    SYS_WRITE,
-    SYS_VERIFY,
     SYS_BINARY,
-    SYS_XRAM,
 } cmd_state;
 
 static uint32_t rw_addr;
 static uint32_t rw_len;
 static uint32_t rw_crc;
-
-static void cmd_ria_read(void)
-{
-    cmd_state = SYS_IDLE;
-    //if (ria_print_error_message())
-    //    return;
-    printf("%04lX", rw_addr);
-    for (size_t i = 0; i < mbuf_len; i++)
-        printf(" %02X", mbuf[i]);
-    printf("\n");
-}
-
-static void cmd_ria_write(void)
-{
-    cmd_state = SYS_IDLE;
-    //if (ria_print_error_message())
-    //    return;
-    cmd_state = SYS_VERIFY;
-    //ria_verify_buf(rw_addr);
-}
-
-static void cmd_ria_verify(void)
-{
-    cmd_state = SYS_IDLE;
-    //ria_print_error_message();
-}
 
 // Commands that start with a hex address. Read or write memory.
 void ram_mon_address(const char *args, size_t len)
@@ -70,7 +40,7 @@ void ram_mon_address(const char *args, size_t len)
     for (; i < len; i++)
         if (args[i] != ' ')
             break;
-    if (rw_addr > 0x1FFFF)
+    if (rw_addr > 0x3FFFF)
     {
         printf("?invalid address\n");
         return;
@@ -78,17 +48,10 @@ void ram_mon_address(const char *args, size_t len)
     if (i == len)
     {
         mbuf_len = (rw_addr | 0xF) - rw_addr + 1;
-        if (rw_addr > 0xFFFF)
-        {
-            printf("%04lX", rw_addr);
-            rw_addr -= 0x10000;
-            for (size_t i = 0; i < mbuf_len; i++)
-                printf(" %02X", xram[rw_addr + i]);
-            printf("\n");
-            return;
-        }
-        //ria_read_buf(rw_addr);
-        cmd_state = SYS_READ;
+        printf("%05lX", rw_addr);
+        for (size_t i = 0; i < mbuf_len; i++)
+            printf(" %02X", xram[rw_addr + i]);
+        printf("\n");
         return;
     }
     uint32_t data = 0x80000000;
@@ -120,20 +83,11 @@ void ram_mon_address(const char *args, size_t len)
                     break;
         }
     }
-    if (rw_addr > 0xFFFF)
+    for (size_t i = 0; i < mbuf_len; i++)
     {
-        rw_addr -= 0x10000;
-        for (size_t i = 0; i < mbuf_len; i++)
-        {
-            xram[rw_addr + i] = mbuf[i];
-            //while (!pix_ready())
-            //    tight_loop_contents();
-            //PIX_SEND_XRAM(rw_addr + i, mbuf[i]);
-        }
-        return;
+        xram[rw_addr + i] = mbuf[i];
     }
-    //ria_write_buf(rw_addr);
-    cmd_state = SYS_WRITE;
+    return;
 }
 
 static void sys_com_rx_mbuf(bool timeout, const char *buf, size_t length)
@@ -146,35 +100,9 @@ static void sys_com_rx_mbuf(bool timeout, const char *buf, size_t length)
         puts("?timeout");
         return;
     }
-    // if (ria_buf_crc32() != rw_crc)
-    // {
-    //     puts("?CRC does not match");
-    //     return;
-    // }
 
-    if (rw_addr >= 0x10000)
-    {
-        cmd_state = SYS_XRAM;
-        for (size_t i = 0; i < rw_len; i++)
-            xram[rw_addr + i - 0x10000] = buf[i];
-    }
-    else
-    {
-        cmd_state = SYS_WRITE;
-        //ria_write_buf(rw_addr);
-    }
-}
-
-static void cmd_xram()
-{
-    while (rw_len)
-    {
-        // if (!pix_ready())
-        //     return;
-        uint32_t addr = rw_addr + --rw_len - 0x10000;
-        // PIX_SEND_XRAM(addr, xram[addr]);
-    }
-    cmd_state = SYS_IDLE;
+    for (size_t i = 0; i < rw_len; i++)
+        xram[rw_addr + i] = buf[i];
 }
 
 void ram_mon_binary(const char *args, size_t len)
@@ -184,14 +112,13 @@ void ram_mon_binary(const char *args, size_t len)
         parse_uint32(&args, &len, &rw_crc) &&
         parse_end(args, len))
     {
-        if (rw_addr > 0x1FFFF)
+        if (rw_addr > 0x3FFFF)
         {
             printf("?invalid address\n");
             return;
         }
         if (!rw_len || rw_len > MBUF_SIZE ||
-            (rw_addr < 0x10000 && rw_addr + rw_len > 0x10000) ||
-            rw_addr + rw_len > 0x20000)
+            rw_addr + rw_len > 0x40000)
         {
             printf("?invalid length\n");
             return;
@@ -211,18 +138,6 @@ void ram_task(void)
     {
     case SYS_IDLE:
     case SYS_BINARY:
-        break;
-    case SYS_READ:
-        cmd_ria_read();
-        break;
-    case SYS_WRITE:
-        cmd_ria_write();
-        break;
-    case SYS_VERIFY:
-        cmd_ria_verify();
-        break;
-    case SYS_XRAM:
-        cmd_xram();
         break;
     }
 }
