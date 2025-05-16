9c04f9d24410e09cae48345801ec55e402c1e399
Sodiumlightbaby <edvard@xox.no>
2025-05-16 21:20:27 +0200

    Optimise Increase PIVIC sys_clk frequencies to 315MHz and 319.2MHz

--- src/firmware/sys/clk.c
+++ src/firmware/sys/clk.c
@@ -9,21 +9,23 @@
 #include "sys/cfg.h"
 #include "vic/vic.h"
 #include "hardware/clocks.h"
+#include "hardware/vreg.h"
 #include <stdbool.h>
 
 void clk_init(void){
 #ifdef PIVIC
+    vreg_set_voltage(VREG_VOLTAGE_1_15);
     switch(cfg_get_mode()){
         case(VIC_MODE_NTSC):
         case(VIC_MODE_TEST_NTSC):
-            set_sys_clock_khz(157500, true);
-            clock_configure(clk_hstx, 0, CLOCKS_CLK_HSTX_CTRL_AUXSRC_VALUE_CLK_SYS, 157500000, 135000000);
+            set_sys_clock_khz(315000, true);
+            clock_configure(clk_hstx, 0, CLOCKS_CLK_HSTX_CTRL_AUXSRC_VALUE_CLK_SYS, 315000000, 135000000);
             break;
         case(VIC_MODE_PAL):
         case(VIC_MODE_TEST_PAL):
         default:
-            set_sys_clock_khz(159600, true);
-            clock_configure(clk_hstx, 0, CLOCKS_CLK_HSTX_CTRL_AUXSRC_VALUE_CLK_SYS, 159600000, 135000000);
+            set_sys_clock_khz(319200, true);
+            clock_configure(clk_hstx, 0, CLOCKS_CLK_HSTX_CTRL_AUXSRC_VALUE_CLK_SYS, 319200000, 135000000);
             break;
     }
 #endif
