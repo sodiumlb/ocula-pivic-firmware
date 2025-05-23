4963a6b1e2fd765658c73c42d9b92894a6e4fca4
Lance Ewing <lance.ewing@gmail.com>
2025-05-24 00:10:42 +0100

    Refactored vic.c to extract the PAL loop into a separate vic_pal.c module.

---
+++ src/firmware/vic/vic_pal.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 dreamseal
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _VIC_PAL_H_
+#define _VIC_PAL_H_
+
+void vic_core1_loop_pal(void);
+ 
+#endif /* _VIC_PAL_H_ */
