c65d3f1967f11a449d32fb0df0f78b9138389591
Sodiumlightbaby <edvard@xox.no>
2025-07-20 17:34:43 +0200

    Fix Make VIC audio integration work with non-cached CRs.

--- src/firmware/vic/vic_ntsc.c
+++ src/firmware/vic/vic_ntsc.c
@@ -827,7 +827,7 @@ void vic_core1_loop_ntsc(void) {
                 }
                 break;
         }
-        aud_tick_inline((uint32_t*)&vic_cr[0xA]);
+        aud_tick_inline((uint32_t*)&vic_cra);
 
         // DEBUG: Temporary check to see if we've overshot the 120 cycle allowance.
         if (pio_interrupt_get(VIC_PIO, 1)) {
--- src/firmware/vic/vic_pal.c
+++ src/firmware/vic/vic_pal.c
@@ -931,7 +931,7 @@ void vic_core1_loop_pal(void) {
                 break;
         }
 
-        aud_tick_inline((uint32_t*)&vic_cr[0xA]);
+        aud_tick_inline((uint32_t*)&vic_cra);
         
         // DEBUG: Temporary check to see if we've overshot the 120 cycle allowance.
         if (pio_interrupt_get(VIC_PIO, 1)) {
